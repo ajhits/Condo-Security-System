ca0dfe022e0d4cb77a131e887074a8bf0503e5c1
unknown <lisboamillen30@gmail.com>
2023-06-08 23:25:09 +0800

    done facial login

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def submit_family():
     
     # Check if the folder already exists
     if os.path.exists(path):
-        return jsonify({'error': f"Folder {path} already exists"}), 400
+        return redirect(url_for('Finger_register'))
     else:
         os.makedirs(path)
         # route to facial registration
@@ -223,61 +223,118 @@ def Finger_register():
 def Facial_Register():
     return render_template('Admin/face-register.html')
 
-# def facialDetection(camera=None, face_detector=None):
-#     global Text
-    
-#     Text=""
-#     B , G , R = (0,255,255)
+# load facial detection
 
-    
-#     textResult = ""
-    
-#     # Login Attempt
-#     success = 0
-#     fail = 0                                    
-    
-#     # Initialize the timer and the start time
-#     timer = 0
-#     start_time = time.time()
-    
-#     while True:
-        
-#         # Capture a frame from the camera
-#         ret, frame = camera.read()
-        
-#         if not ret:
-#             break
+result = "capturing..."
 
-#         frame = cv2.flip(frame,1)
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-#         # Detect faces in the frame
-#         faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+@app.route('/facial_register')
+def facial_register():
+ 
+    return Response(
+        facialRegister(
+        camera=camera, 
+        face_detector=face_detection,
+        path=path
+        ),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#  Facial Register Function
+def facialRegister(camera=None, face_detector=None, path=None):
+    global result
+    capture=1
+    result = "Capture " + str(capture)
+    
+    # Initialize the timer and the start time
+    timer = 0
+    start_time = time.time()
    
-#         # # checking detecting face should be 1
-#         # if len(faces) == 1:
-#         #          = faces[0]
-#         for (x, y, w, h) in faces:
-                            
-
-#             # Get the coordinates of the face,draw rectangele and put text
-            
+    while True:
         
-#             cv2.rectangle(frame, (x, y), (x+w, y+h), (B,G,R), 2)
-#             cv2.putText(frame,textResult,(x,y+h+30),cv2.FONT_HERSHEY_COMPLEX,1,(B,G,R),1)
+        # Capture a frame from the camera
+        ret, frame = camera.read()
+        
+        # check if camera is working
+        if not ret:
+            break
+     
+        frame = cv2.flip(frame,1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+        # Detect faces in the frame
+        faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=20, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE) 
+        
+        # check if there is 1 face detected
+        # NOTE: if more than 1 it wont detected and the timer will restart
+        if len(faces) == 1:
+        
+    
+            if timer >= 0.5:
+                
+                if not len(os.listdir(path))==10:
+                    cv2.imwrite(path + "/" +str(capture)+".png", frame)
+                    capture+=1
+                    result = "Capture " + str(capture)
 
-#         # elif len(faces) > 1:
+                    print(result)
+                else:
+                    
+                    result = "Training"
+
+                    print(result)
+                    # # facial training
+                    # result=Jolo().Face_Train()
+
+                    
+                    
+                timer = 0
+                start_time = time.time()
+            else:
+                timer += time.time() - start_time
+                start_time = time.time()
+                
+            # Get the coordinates of the face
+            (x, y, w, h) = faces[0]
             
-#         #     # If more than 1 faces 
-#         #     # B, G, R = (0, 0, 255)
-#         #     Text = "More than 1 face is detected"
-#         # else:
-#         #     # B, G, R = (0, 0, 0)
-#         #     Text = "No face is detected"    
-            
-#         _, frame_encoded  = cv2.imencode('.png', frame)
-#         yield (b'--frame\r\n'
-#                 b'Content-Type: image/jpeg\r\n\r\n' + frame_encoded.tobytes() + b'\r\n')
+            # Draw a rectangle around the face and pt text
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)    
+        
+        
+        _, frame_encoded  = cv2.imencode('.png', frame)
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame_encoded.tobytes() + b'\r\n')
+
+# ********************* API for Facial Register status
+@app.route("/update_variable", methods=["GET"])
+def update_variable():
+    # Replace this with the code that generates the new value of the variable
+    new_value = result
+    return jsonify(result)
+
+
+# ------------------- Facial Training
+@app.route('/Training')
+def Training():
+    return render_template('loading.html')
+
+
+# ********************* API for Facial training
+
+def trainingtraining():
+    global result
+    result = Jolo().Face_Train()
+
+@app.route("/facialTraining", methods=["GET"])
+def facialTraining():
+
+    result = Jolo().Face_Train()
+    return jsonify(result)
+
+    # # Replace this with the code that generates the new value of the variable
+    # if result == "Successfully trained":
+    #     print(result)
+    #     return jsonify(result)
+    # else:
+        
+    #     return jsonify("Don't Interrupt Training Process")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~ Database API
 
